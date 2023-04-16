ce026aab13e45853fd7415447f5975d2b72a9f3d
Nendhx <126443413+Nendhx@users.noreply.github.com>
2023-04-16 15:57:23 +0000

    change

--- shop.js
+++ shop.js
@@ -1,9 +1,47 @@
-function Show() {
-  let Snickers = document.getElementById("SnickersId").value;
-  let MilkyWay = document.getElementById("MilkyWayId").value;
-  let Mars = document.getElementById("MarsId").value;
-  let Twixx = document.getElementById("TwixxsId").value;
-  let cost = 0;
-  cost = Snickers * 1700 + MilkyWay * 1800 + Mars * 1900 + Twixx * 2000;
-  document.getElementById("result").innerHTML = cost;
+const products = [
+  {
+    name: "snickers",
+    price: 1000,
+    quantity: 0,
+  },
+  {
+    name: "milkyway",
+    price: 1200,
+    quantity: 0,
+  },
+  {
+    name: "mars",
+    price: 1500,
+    quantity: 0,
+  },
+  {
+    name: "twix",
+    price: 1600,
+    quantity: 0,
+  },
+  {
+    name: "kitkat",
+    price: 1300,
+    quantity: 0,
+  },
+];
+
+function Add(pname) {
+  for(let i = 0; i < products.length; i++){
+    if(products[i].name === pname) products[i].quantity++;
+  }
+}
+
+function Reduce(pname) {
+  for(let i = 0; i < products.length; i++){
+    if(products[i].name === pname && products[i].quantity > 0) products[i].quantity--;
+  }
+}
+
+function Purchase() {
+  let total = 0;
+  for (let i = 0; i < products.length; i++) {
+    total += products[i].price * products[i].quantity;
+  }
+  document.getElementById("total").innerHTML = total;
 }
